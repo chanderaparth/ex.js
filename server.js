d074251dc1bf0a90455d1a1e90195242566e76ed
parth <chnderachanderaparth986@gmail.com>
2023-12-05 12:29:00 +0530

    js

--- server.js
+++ server.js
@@ -1,58 +1,58 @@
-// const express = require('express');
-// const server = express(); // create server;
+const express = require('express');
+const server = express(); // create server;
 
-// const port = '5555';
-// const fs = require('fs');
-// const product = require('./products.json');
-// const { listen } = require('express/lib/application');
-// const { connect } = require('http2');
-// const { contentType } = require('express/lib/response');
-
-// server.get('/',(req,res)=>{
-//     res.send("Hello world parth...");
-// })
-
-// server.get('/',(req,res)=>{
-//     res.send(product);
-// })
-// server.get('/product/:username',(req,res)=>{
-
-//     res.send(`Hello world from ${req.params.username}`)
-// })
-// server.listen(port, ()=>{
-//     console.log(`Server start at ${port}`);
-// })
-
-const { error } = require('console');
-const express = require('express')
-const fs = require('fs')
-
-const product = require('./carts.json')
-const port = 8080 ;
-
-const server = express();
+const port = '5555';
+const fs = require('fs');
+const product = require('./products.json');
+const { listen } = require('express/lib/application');
+const { connect } = require('http2');
+const { contentType } = require('express/lib/response');
 
 server.get('/',(req,res)=>{
-
-    res.send("Hello World From Hitesh...")                                                  
+    res.send("Hello world parth...");
 })
 
-server.get('/product',(req,res)=>{
+server.get('/',(req,res)=>{
+    res.send(product);
+})
+server.get('/product/:username',(req,res)=>{
 
-    res.send(product)
+    res.send(`Hello world from ${req.params.username}`)
+})
+server.listen(port, ()=>{
+    console.log(`Server start at ${port}`);
 })
 
- server.get('/product/:username',(req,res)=>{
+// const { error } = require('console');
+// const express = require('express')
+// const fs = require('fs')
 
-      res.send(`Hello world from ${req.params.username}`)
- })
-server.listen(port,(err)=>{
+// const product = require('./carts.json')
+// const port = 8080 ;
 
-    if(err){
-        console.log(err)
-        }
-        else{
+// const server = express();
 
-            console.log("Server started...")
-        }
-})
+// server.get('/',(req,res)=>{
+
+//     res.send("Hello World From Hitesh...")                                                  
+// })
+
+// server.get('/product',(req,res)=>{
+
+//     res.send(product)
+// })
+
+//  server.get('/product/:username',(req,res)=>{
+
+//       res.send(`Hello world from ${req.params.username}`)
+//  })
+// server.listen(port,(err)=>{
+
+//     if(err){
+//         console.log(err)
+//         }
+//         else{
+
+//             console.log("Server started...")
+//         }
+// })
