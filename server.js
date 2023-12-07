0650c7d5353fa90afe18666e7982fe6a42d9cebf
parth <chnderachanderaparth986@gmail.com>
2023-12-07 17:55:56 +0530

    js

--- server.js
+++ server.js
@@ -1,24 +1,49 @@
 const express = require('express');
 const server = express(); // create server;
-
 const port = '5555';
 const fs = require('fs');
-const product = require('./products.json');
-const { listen } = require('express/lib/application');
-const { connect } = require('http2');
-const { contentType } = require('express/lib/response');
+const product = require('./public/products.json');
+const morgan = require('morgan');
+// const { listen } = require('express/lib/application');
+// const { connect } = require('http2');
+// const { contentType } = require('express/lib/response');
 
-server.get('/',(req,res)=>{
-    res.send("Hello world parth...");
-})
+// 1.
+// server.get('/',(req,res)=>{
+    //     res.send("Hello world parth...");
+    // })
+    
+    // server.get('/',(req,res)=>{
+        //     res.send(product);
+        // })
+        // server.get('/product/:username',(req,res)=>{
+            
+            //     res.send(`Hello world from ${req.params.username}`)
+            // })
 
-server.get('/',(req,res)=>{
-    res.send(product);
-})
-server.get('/product/:username',(req,res)=>{
+// 2.
+    // middleware
+    server.use(morgan('dev'))
+    server.use(express.json());
 
-    res.send(`Hello world from ${req.params.username}`)
-})
+    // create,rade,update,delete //data 
+   // create products => /products
+   server.post('/products',(req,res)=>
+   {
+    product.push(req.body)
+    // res.json({Message:'product is addd',product: req.body})
+    res.json(product)
+   })
+
+   server.get('/product/:id',(req,res)=>{
+    // const id = +req.params.id; number valu 
+    const id = req.params.id;
+    const item = product.find((p)=>p.id===id)
+    res.status(200).json(item);
+   })
+
+
+
 server.listen(port, ()=>{
     console.log(`Server start at ${port}`);
 })
