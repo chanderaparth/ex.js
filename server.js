cd7d76884cc55753d0067c9c4d95b2b5a6066534
parth <chnderachanderaparth986@gmail.com>
2023-12-09 17:46:49 +0530

    js

--- server.js
+++ server.js
@@ -34,13 +34,50 @@ const morgan = require('morgan');
     // res.json({Message:'product is addd',product: req.body})
     res.json(product)
    })
+  // specific products => /products/:id   
+//    server.get('/product/:id',(req,res)=>{
+//     // const id = +req.params.id; number valu 
+//     const id = req.params.id;
+//     const item = product.find((p)=>p.id===id)
+//     res.status(200).json(item);
+//    })
 
-   server.get('/product/:id',(req,res)=>{
-    // const id = +req.params.id; number valu 
+
+//    replace products => /products/:id 
+server.post('/',(req,res,next)=>{
+    res.json(product);
+    next();
+})
+// server.patch('/product/:id',(req,res)=>
+// {
+//     const id = req.params.id;
+//     const itemindex = product.findIndex((p)=>p.id===id)
+//     product.splice(itemindex,1,{...req.body,id:id})
+//     res.status(200).json({Message: "product is replace"});
+// })
+
+//    update products => /products/:id
+// server.patch('/product/:id',(req,res)=>
+// {
+//     const id = req.params.id;
+//     const itemindex = product.findIndex((p)=>p.id===id)
+//     let item = product[itemindex]; 
+//     product.splice(itemindex,1,{...item,...req.body})
+//     res.status(200).json({Message: "product is update",product: item});
+// })
+
+// delete
+
+server.delete('/product/:id',(req,res)=>
+{
     const id = req.params.id;
-    const item = product.find((p)=>p.id===id)
-    res.status(200).json(item);
-   })
+    const itemindex = product.findIndex((p)=>p.id===id)
+    let item = product[itemindex]; 
+    product.splice(itemindex,1);
+    res.status(200).json({Message: "product is delete",product: item});
+})
+
+
 
 
 
